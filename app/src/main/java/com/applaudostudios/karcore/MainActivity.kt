b7862d32a88d94991350e5619088338eaf606c88
jarteaga-applaudo <jarteaga@applaudostudios.com>
2019-07-25 12:03:04 -0600

    :sparkles: add functions to add anchors and create node for each figure

--- app/src/main/java/com/applaudostudios/karcore/MainActivity.kt
+++ app/src/main/java/com/applaudostudios/karcore/MainActivity.kt
@@ -9,4 +9,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, modelUri: Uri) {
+        val modelRenderable = ModelRenderable.builder()
+            .setSource((fragment.requireContext()), modelUri)
+            .build()
+
+        modelRenderable.thenAccept { renderableObject -> addNodeToScene(fragment, anchor, renderableObject) }
+
+        modelRenderable.exceptionally {
+            val toast = Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT)
+            toast.show()
+            null
+        }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, renderableObject: Renderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderableObject
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+    private fun setModelPath(modelFileName: String) {
+        selectedObject = Uri.parse(modelFileName)
+        val toast = Toast.makeText(applicationContext, "bigLamp", Toast.LENGTH_SHORT)
+        toast.show()
+    }
 }
